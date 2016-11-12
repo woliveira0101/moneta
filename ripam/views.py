ada4ad26e8508014f857496cf782bacc97b4572a
Chaoyi Zha <summermontreal@gmail.com>
2016-11-12 16:20:52 -0500

    Add styles & set up layout

--- ripam/views.py
+++ ripam/views.py
@@ -1,5 +1,15 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth import authenticate
 
-# Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def login(request):
+    if request.method != "POST":
+        return HttpResponse('Error')
+
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+
+    user = authenticate(username=username, password=password)
